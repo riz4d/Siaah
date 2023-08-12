7e6e143697699c495782aaaf85cd2b96a69589b6
Mohamed Rizad <22ad063@excelcolleges.com>
2023-08-12 18:01:26 +0530

    post file

--- modules/ig/post.py
+++ modules/ig/post.py
@@ -3,17 +3,19 @@ import os
 from instaloader import Instaloader, Post
 loader = Instaloader(download_videos=True, download_geotags=False, download_comments=False, compress_json=False)
 
-def download_instagram_video(url):
-    match = re.search(r"(?:https?:\/\/)?(?:www\.)?(?:instagram\.com)\/(?:reel)\/([^\/?]+)", url)
+
+def download_instagram_post(url):
+    match = re.search(r"(?:https?:\/\/)?(?:www\.)?(?:instagram\.com|instagr\.am)\/(?:p|reel)\/([^\/?]+)", url)
     if not match:
         print("Invalid Instagram URL")
     shortcode = match.group(1)
     post = Post.from_shortcode(loader.context, shortcode)
-    loader.download_post(post, target="ig_videos")
+    loader.download_post(post, target="ig_images")
     media_file = None
-    for file in os.listdir("ig_videos"):
+    for file in os.listdir("ig_images"):
         print(file)
-        if file.endswith(".mp4"):
-            media_file = "ig_videos/"+file
+        if file.endswith(".jpg"):
+            media_file = "ig_images/"+file
             break
     return media_file
+
