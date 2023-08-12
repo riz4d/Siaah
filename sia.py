fe043f3be6489abcc830b3d5cd785ec222cbdf9d
Mohamed Rizad <22ad063@excelcolleges.com>
2023-08-12 17:59:24 +0530

    Updt

--- sia.py
+++ sia.py
@@ -26,10 +26,10 @@ else:
     bot_token= config.bot_token
         
 if config.api_id=="":
-    api_id = os.environ.get("API_ID")
+    api_id = int(os.environ.get("API_ID"))
 else:
-    api_id = config.api_id
-        
+    api_id = int(config.api_id)
+       
 if config.api_hash=="":
     api_hash = os.environ.get("API_HASH")
 else:
